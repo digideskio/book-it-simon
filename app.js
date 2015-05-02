b6ca1b26015ee30d0a069e373e28d4340ff91631
wthuang <weiteng.huang@concur.com>
2015-05-02 19:57:53 -0400

    add route
    add route
    
    Signed-off-by: wthuang <weiteng.huang@concur.com>

--- app.js
+++ app.js
@@ -19,9 +19,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/tools', express.static(__dirname + '/tools'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+// app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
