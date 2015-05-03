eaf4c26b14c76db1301ed1a7bf2454e22a9b9c93
Dustin Graves <dustin.graves@concur.com>
2015-05-02 23:07:48 -0400

    fixed public

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use('/tools', express.static(__dirname + '/tools'));
 app.use('/app', express.static(__dirname + '/app'));
 app.use('/public', express.static(__dirname + '/public'));
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
