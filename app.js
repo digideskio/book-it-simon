6d3c36526e551d9d85b77c88a32d120d60790d5f
wthuang <weiteng.huang@concur.com>
2015-05-02 21:21:40 -0400

    fix incorrect route
    fix incorrect route
    
    Signed-off-by: wthuang <weiteng.huang@concur.com>

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/tools', express.static(__dirname + '/tools'));
-app.use('/app', express.static(__dirname + '/tools'));
+app.use('/app', express.static(__dirname + '/app'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
